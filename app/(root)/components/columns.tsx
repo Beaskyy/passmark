3f6eef48cb0b2fbd55b764e0105b97fba1f1c390
Beaskyy <afeez.akintobi@yahoo.com>
2025-06-06 16:40:48 +0100

    UI update

--- app/(root)/components/columns.tsx
+++ app/(root)/components/columns.tsx
@@ -1,30 +1,138 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { ColumnDef } from "@tanstack/react-table";
-import { ArrowUp } from "lucide-react";
 import Image from "next/image";
-import { PaymentsCellAction } from "./PaymentsCellAction";
+import { MarkedScript } from "@/lib/data";
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { useRouter } from "next/navigation";
-
-export type PaymentsColumn = {
-  id: number;
-  payment_id: string;
-  amount: string;
-  date: string;
-  time: string;
-  association: string;
-  customer: string;
-  created_at: string;
-  status: string;
-};
-
-// The columns array definition will be moved to app/(root)/page.tsx
-// export const columns: ColumnDef<PaymentsColumn>[] = [...];
+export const columns: ColumnDef<MarkedScript>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <div className="flex items-center  !border-[#E1E4EA] rounded">
+        <input
+          type="checkbox"
+          checked={table.getIsAllRowsSelected()}
+          onChange={table.getToggleAllRowsSelectedHandler()}
+          className="w-4 h-4 border border-[#E1E4EA] rounded cursor-pointer shadow-sm"
+        />
+      </div>
+    ),
+    cell: ({ row }) => (
+      <div
+        className="flex items-center  !border-[#E1E4EA] rounded"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <input
+          type="checkbox"
+          checked={row.getIsSelected()}
+          onChange={row.getToggleSelectedHandler()}
+          className="w-4 h-4  !border-[#dd263b] rounded cursor-pointer"
+        />
+      </div>
+    ),
+  },
+  {
+    accessorKey: "scriptUploaded",
+    header: ({ column }) => {
+      return (
+        <div
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          className="flex items-center gap-0.5 cursor-pointer"
+        >
+          Script Uploaded
+          <Image
+            src="/images/up-down-fill.svg"
+            alt="up-down-fill"
+            width={20}
+            height={20}
+          />
+        </div>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="flex items-center gap-0.5">
+        <Image src="/images/png.svg" alt="doc" width={32} height={32} />
+        <div>
+          <p className="font-medium">{row.original?.scriptUploaded}</p>
+          <small className="text-xs text-[#5C5C5C]">2.4 MB</small>
+        </div>
+      </div>
+    ),
+  },
+  {
+    accessorKey: "studentId",
+    header: ({ column }) => {
+      return (
+        <div
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          className="flex items-center gap-0.5 cursor-pointer"
+        >
+          Student's ID
+          <Image
+            src="/images/up-down-fill.svg"
+            alt="up-down-fill"
+            width={20}
+            height={20}
+          />
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "courseCode",
+    header: ({ column }) => {
+      return (
+        <div
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          className="flex items-center gap-0.5 cursor-pointer"
+        >
+          Course Code
+          <Image
+            src="/images/up-down-fill.svg"
+            alt="up-down-fill"
+            width={20}
+            height={20}
+          />
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "dateMarked",
+    header: ({ column }) => {
+      return (
+        <div
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          className="flex items-center gap-0.5 cursor-pointer"
+        >
+          Date Marked
+          <Image
+            src="/images/up-down-fill.svg"
+            alt="up-down-fill"
+            width={20}
+            height={20}
+          />
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "status",
+    header: ({ column }) => {
+      return (
+        <div
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          className="flex items-center gap-0.5 cursor-pointer"
+        >
+          Status
+          <Image
+            src="/images/up-down-fill.svg"
+            alt="up-down-fill"
+            width={20}
+            height={20}
+          />
+        </div>
+      );
+    },
+  },
+];
